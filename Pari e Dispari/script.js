374ba769cf2329561b982adfed000d0424d0e1a9
Ortello7 <domenicoortello00@gmail.com>
2024-12-04 16:52:28 +0100

    commit for ticket

--- Pari e Dispari/script.js
+++ Pari e Dispari/script.js
@@ -3,3 +3,22 @@
 //Sommiamo i due numeri.
 //Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione).
 //Dichiariamo chi ha vinto.
+
+// L'utente sceglie tra pari o dispari
+const sceltaUtente = prompt("Scegli tra pari o dispari!");
+
+// L'utente sceglie un numero (da 1 a 5)
+const numeroUtente = Number(prompt("Scegli un numero da 1 a 5!"));
+
+//Creare la funzione per generare un numero random per la cpu (da 1 a 5)
+function generareNumeroCpu() {
+    const numeroCpu = Math.floor(Math.random() * 5) + 1;
+    return numeroCpu;
+}
+
+// Mostro il numero della Cpu
+alert("Il numero della Cpu è: " + generareNumeroCpu());
+
+//Sommiamo i due numeri
+const sommaNumeri = generareNumeroCpu() + numeroUtente;
+alert ("La somma dei numeri è: " + sommaNumeri);
